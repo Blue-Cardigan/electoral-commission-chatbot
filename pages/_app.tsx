923ddb9ec3623601347febd2b27d02d1fbd35859
Jethro Reeve <jethro.reeve@gmail.com>
2024-08-29 18:54:45 +0100

    made it build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,8 @@
-import '@/styles/base.css';
 import type { AppProps } from 'next/app';
-import { Html } from 'next/document';
-import Head from 'next/head';
-
-// const inter = Inter({
-//   variable: '--font-inter',
-//   subsets: ['latin'],
-// });
+import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Electoral Comission Chatbot</title>
-        <meta
-          name="viewport"
-          content="width=device-width,minimum-scale=1,initial-scale=1"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
