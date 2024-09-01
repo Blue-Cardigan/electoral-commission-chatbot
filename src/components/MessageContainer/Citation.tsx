82b0d737d4197036c26e68b2941039c963e258ee
Jethro Reeve <jethro.reeve@gmail.com>
2024-09-01 12:08:53 +0100

    fix links

--- src/components/MessageContainer/Citation.tsx
+++ src/components/MessageContainer/Citation.tsx
@@ -11,10 +11,7 @@ export const Citation: React.FC<CitationProps> = ({ index, url, onClick }) => {
     <sup>
       <a
         href={url}
-        onClick={(e) => {
-          e.preventDefault();
-          onClick();
-        }}
+        onClick={onClick} // Removed e.preventDefault()
         className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
       >
         [{index}]
